d38af748778f6f5ba9ea712b7e385b878405a294
rodrigo.legaria@millicom.com <rodrigo.legaria@millicom.com>
2023-03-22 14:55:22 -0600

    My first action is ready

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ try {
     version[2] = parseInt(version[2]) + increment;
   else if (level_of_change == "minor")
     version[1] = parseInt(version[1]) + increment;
-  else if (level_of_change == "major" || level_of_change == "new")
+  else if (level_of_change == "major") 
+    version[0] = parseInt(version[0]) + increment;
+  else if (level_of_change == "new")
     version[0] = parseInt(version[0]) + increment;
   else 
     core.setFailed("Invalid level of change");
