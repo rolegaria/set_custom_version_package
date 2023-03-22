d7af4bd42b8cec30d545a99eb78289abd401ae5a
rodrigo.legaria@millicom.com <rodrigo.legaria@millicom.com>
2023-03-22 14:46:44 -0600

    My first action is ready

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const core = require("@actions/core");
 try {
   const current_version = core.getInput("current-version");
   const level_of_change = core.getInput("level-of-change");
+  const increment = 1;
 
   let version = current_version.split(".");
 
   if (level_of_change == "patch")
-    version[2] += 1;
+    version[2] = parseInt(version[2]) + increment;
   else if (level_of_change == "minor")
-    version[1] +=1;
-  else if (level_of_change == "major")
-    version[0] +=1;
+    version[1] = parseInt(version[1]) + increment;
+  else if (level_of_change == "major" || level_of_change == "new")
+    version[0] = parseInt(version[0]) + increment;
   else 
     core.setFailed("Invalid level of change");
 
